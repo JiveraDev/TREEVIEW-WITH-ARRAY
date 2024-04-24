f9be50a0bbe4e2b49c309d99031cc1715c12cc03
J-R <lorenceolivera1039@gmail.com>
2024-04-24 10:31:06 +0800

    Add project files.

---
+++ TREEVIEW WITH ARRAY/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dwarfplanet = New System.Windows.Forms.Button()
+        Me.planet = New System.Windows.Forms.Button()
+        Me.frsttreeview = New System.Windows.Forms.TreeView()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(366, 214)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 17)
+        Me.Label2.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(356, 214)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "CLEAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(162, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(212, 22)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "PLANET MOONTREE VIEW"
+        '
+        'dwarfplanet
+        '
+        Me.dwarfplanet.Location = New System.Drawing.Point(306, 143)
+        Me.dwarfplanet.Name = "dwarfplanet"
+        Me.dwarfplanet.Size = New System.Drawing.Size(181, 35)
+        Me.dwarfplanet.TabIndex = 8
+        Me.dwarfplanet.Text = "LOAD DWARF PLANET"
+        Me.dwarfplanet.UseVisualStyleBackColor = True
+        '
+        'planet
+        '
+        Me.planet.Location = New System.Drawing.Point(339, 102)
+        Me.planet.Name = "planet"
+        Me.planet.Size = New System.Drawing.Size(129, 35)
+        Me.planet.TabIndex = 7
+        Me.planet.Text = "LOAD PALNETS"
+        Me.planet.UseVisualStyleBackColor = True
+        '
+        'frsttreeview
+        '
+        Me.frsttreeview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.frsttreeview.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.frsttreeview.Location = New System.Drawing.Point(35, 78)
+        Me.frsttreeview.Name = "frsttreeview"
+        Me.frsttreeview.Size = New System.Drawing.Size(265, 595)
+        Me.frsttreeview.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 712)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dwarfplanet)
+        Me.Controls.Add(Me.planet)
+        Me.Controls.Add(Me.frsttreeview)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dwarfplanet As Button
+    Friend WithEvents planet As Button
+    Friend WithEvents frsttreeview As TreeView
+End Class
---
+++ TREEVIEW WITH ARRAY/Form1.vb
@@ -0,0 +1,245 @@
+﻿Public Class Form1
+    Dim arrPlanets(7) 'Planets
+    Dim arrDwarfPlanets(4) ' Dwarf Planets
+    Dim arrMoons() 'Moons
+
+    Dim strPlanet As String
+    Dim strDwarfPlanet As String
+
+    Private Sub planet_Click(sender As Object, e As EventArgs) Handles planet.Click
+        'Load All Planets Into Array
+        arrPlanets(0) = "Mercury"
+        arrPlanets(1) = "Venus"
+        arrPlanets(2) = "Earth"
+        arrPlanets(3) = "Mars"
+        arrPlanets(4) = "Jupiter"
+        arrPlanets(5) = "Saturn"
+        arrPlanets(6) = "Uranus"
+        arrPlanets(7) = "Neptune"
+        For Each strPlanet In arrPlanets
+
+            frsttreeview.Nodes.Add(strPlanet)
+        Next
+
+
+    End Sub
+
+    Private Sub dwarfplanet_Click(sender As Object, e As EventArgs) Handles dwarfplanet.Click
+        'Load All Dwarf Planets into Array
+        arrDwarfPlanets(0) = "Pluto"
+        arrDwarfPlanets(1) = "Ceres"
+        arrDwarfPlanets(2) = "Eris"
+        arrDwarfPlanets(3) = "Makemake"
+        arrDwarfPlanets(4) = "Haumea"
+
+        For Each strDwarfPlanet In arrDwarfPlanets
+
+            frsttreeview.Nodes.Add(strDwarfPlanet)
+
+        Next
+    End Sub
+
+    Private Sub frsttreeview_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles frsttreeview.AfterSelect
+
+        If frsttreeview.SelectedNode IsNot Nothing Then
+            Dim strSeltext As String = frsttreeview.SelectedNode.Text 'Get Selected Node Text
+
+            Select Case strSeltext
+
+                Case "Mercury"
+
+                    frsttreeview.SelectedNode.Nodes.Add("No Moon")
+
+                Case "Venus"
+
+                    frsttreeview.SelectedNode.Nodes.Add("No Moon")
+
+                Case "Earth"
+
+                    frsttreeview.SelectedNode.Nodes.Add("The Moon")
+
+                Case "Mars"
+
+                    'Redimension Moon Array, Without preserving Its Contents
+                    ReDim arrMoons(1) 'Two Elements
+
+                    arrMoons(0) = "Phobos"
+                    arrMoons(1) = "Deimos"
+
+                    Dim i As Integer 'Loop Counter
+
+                    For i = 0 To arrMoons.Length - 1 'Loop Through All Array Elements
+
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i)) 'Add To Selected Node
+
+                    Next i
+
+                Case "Jupiter"
+
+                    ReDim arrMoons(15) 'Jupiter's 16 Moons
+
+                    arrMoons(0) = "Metis"
+                    arrMoons(1) = "Adrastea"
+                    arrMoons(2) = "Amalthea"
+                    arrMoons(3) = "Thebe"
+                    arrMoons(4) = "Io"
+                    arrMoons(5) = "Europa"
+                    arrMoons(6) = "Ganymede"
+                    arrMoons(7) = "Callisto"
+                    arrMoons(8) = "Leda"
+                    arrMoons(9) = "Himalia"
+                    arrMoons(10) = "Lysithea"
+                    arrMoons(11) = "Elara"
+                    arrMoons(12) = "Ananke"
+                    arrMoons(13) = "Carme"
+                    arrMoons(14) = "Pasiphae"
+                    arrMoons(15) = "Sinope"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i)) 'Add
+
+                    Next i
+                Case "Saturn"
+
+                    ReDim arrMoons(17)
+
+                    arrMoons(0) = "Pan"
+                    arrMoons(1) = "Atlas"
+                    arrMoons(2) = "Prometheus"
+                    arrMoons(3) = "Pandora"
+                    arrMoons(4) = "Epimetheus"
+                    arrMoons(5) = "Janus"
+                    arrMoons(6) = "Mimas"
+                    arrMoons(7) = "Enceladus"
+                    arrMoons(8) = "Tethys"
+                    arrMoons(9) = "Telesto"
+                    arrMoons(10) = "Calypso"
+                    arrMoons(11) = "Dione"
+                    arrMoons(12) = "Helene"
+                    arrMoons(13) = "Rhea"
+                    arrMoons(14) = "Titan"
+                    arrMoons(15) = "Hyperion"
+                    arrMoons(16) = "Iapetus"
+                    arrMoons(17) = "Phoebe"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        'Add All Saturn Moons
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i))
+
+                    Next i
+
+                Case "Uranus"
+
+                    ReDim arrMoons(20)
+
+                    arrMoons(0) = "Cordelia"
+                    arrMoons(1) = "Ophelia"
+                    arrMoons(2) = "Bianca"
+                    arrMoons(3) = "Cressida"
+                    arrMoons(4) = "Desdemona"
+                    arrMoons(5) = "Juliet"
+                    arrMoons(6) = "Portia"
+                    arrMoons(7) = "Rosalind"
+                    arrMoons(8) = "Belinda"
+                    arrMoons(9) = "Puck"
+                    arrMoons(10) = "Miranda"
+                    arrMoons(11) = "Ariel"
+                    arrMoons(12) = "Umbriel"
+                    arrMoons(13) = "Titania"
+                    arrMoons(14) = "Oberon"
+                    arrMoons(15) = "Caliban"
+                    arrMoons(16) = "Sycorax"
+                    arrMoons(17) = "Prospero"
+                    arrMoons(18) = "Setebos"
+                    arrMoons(19) = "Stephano"
+                    arrMoons(20) = "Trinculo"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        'Add All Uranus Moons
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i))
+
+                    Next i
+
+                Case "Neptune"
+
+                    ReDim arrMoons(7)
+
+                    arrMoons(0) = "Naiad"
+                    arrMoons(1) = "Thalassa"
+                    arrMoons(2) = "Despina"
+                    arrMoons(3) = "Galatea"
+                    arrMoons(4) = "Larissa"
+                    arrMoons(5) = "Proteus"
+                    arrMoons(6) = "Triton"
+                    arrMoons(7) = "Nereid"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i))
+
+                    Next i
+
+                Case "Pluto" 'Dwarf Planet
+
+                    ReDim arrMoons(2)
+
+                    arrMoons(0) = "Charon"
+                    arrMoons(1) = "Nix"
+                    arrMoons(2) = "Hydra"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i))
+
+                    Next i
+
+                Case "Ceres" 'Dwarf Planet
+
+                    frsttreeview.SelectedNode.Nodes.Add("No Moon")
+
+                Case "Eris"
+
+                    ReDim arrMoons(0)
+
+                    arrMoons(0) = "Dysnomia"
+
+                    frsttreeview.SelectedNode.Nodes.Add("Dysnomia") 'Not Needing a Loop For 1 Item!
+
+                Case "Makemake"
+
+                    frsttreeview.SelectedNode.Nodes.Add("No Moon")
+
+                Case "Haumea"
+
+                    ReDim arrMoons(1)
+
+                    arrMoons(0) = "Hi'iaka"
+                    arrMoons(1) = "Namaka"
+
+                    Dim i As Integer
+
+                    For i = 0 To arrMoons.Length - 1
+
+                        frsttreeview.SelectedNode.Nodes.Add(arrMoons(i))
+                    Next i
+            End Select
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        frsttreeview.Nodes.Clear()
+    End Sub
+End Class
